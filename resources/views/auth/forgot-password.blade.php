71a9b1fffa50f90571d676b9973d69f29a68dc46
bilalbajou <bajoubilal34@gmail.com>
2022-01-21 16:28:37 +0100

    traduction v4

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,11 +1,12 @@
 <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
-            <x-jet-authentication-card-logo />
+            {{-- <x-jet-authentication-card-logo /> --}}
+            <img src="assets/images/full_logo.png">  
         </x-slot>
 
         <div class="mb-4 text-sm text-gray-600">
-            {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+            {{ __('Mot de passe oublié ? Ce n\'est rien. Faites-nous simplement connaître votre adresse e-mail et nous vous enverrons un lien de réinitialisation de mot de passe qui vous permettra de choisir un nouveau.') }}
         </div>
 
         @if (session('status'))
@@ -26,7 +27,7 @@
 
             <div class="flex items-center justify-end mt-4">
                 <x-jet-button>
-                    {{ __('Email Password Reset Link') }}
+                    {{ __('Envoyer') }}
                 </x-jet-button>
             </div>
         </form>
