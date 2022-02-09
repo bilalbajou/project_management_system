732bf28aa976fb1baef556d434ea2ca932ff3845
bilalbajou <bajoubilal34@gmail.com>
2022-02-09 20:32:07 +0100

    add migration user_type

---
+++ database/migrations/2022_02_09_190520_add_user_type.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUserType extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            Schema::table('users', function (Blueprint $table) {
+                $table->enum('user_type', ['Chef du projet', 'Webmaster','Employée']);
+            });
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->enum('user_type', ['Chef du projet', 'Webmaster','Employée']);
+        });
+         
+    }
+}
